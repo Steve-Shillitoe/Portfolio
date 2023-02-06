e2bbf2961e902e348e626024006a1441304630f5
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2023-02-06 19:01:23 +0000

    Add project files.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Steve Shillitoe\source\repos\Portfolio\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
